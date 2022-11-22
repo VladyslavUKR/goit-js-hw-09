65a1558abd9104efc639c34632c8d67a16666a7b
Illia666 <108007460+Illia666@users.noreply.github.com>
2022-11-22 16:27:38 +0200

    az

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,25 +1,52 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-console.log(galleryItems);
 
-const gallery = document.querySelector('.gallery');
-
-const createMarkupGallery = () => {
+const galleryMarkup = createGalleryCardMarkup(galleryItems);
+function createGalleryCardMarkup(galleryItem) {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `<a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description} " />
-</a>`;
+      return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div>`;
     })
-    .join('\n');
-};
+    .join('');
+}
 
-console.log(createMarkupGallery());
-gallery.innerHTML = createMarkupGallery();
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup);
 
-var lightbox = new SimpleLightbox('.gallery a', {
-  /* options */
+galleryEl.addEventListener('click', onGalleryClick);
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  `<div class="gallery">
+    <a href="${event.target.getAttribute(
+      'href'
+    )}"><img src="${event.target.getAttribute(
+    'src'
+  )}" alt="${event.target.getAttribute('alt')}" /></a>
+    <a href="${event.target.getAttribute(
+      'href'
+    )}"><img src="${event.target.getAttribute(
+    'src'
+  )}" alt="${event.target.getAttribute('alt')}"/></a>
+</div>
+    `;
+}
+let gallery = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
+  showCounter: false,
 });
+
+gallery.on('show.simplelightbox');
